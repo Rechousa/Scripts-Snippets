59386777899b78117a51ea70dfc43025c106ab67
Pedro Martins <rechousa@gmail.com>
2019-09-19 15:52:12 +0100

    Added batch script to run command line as a different user account

---
+++ Batch/run cmd as different user.bat
@@ -0,0 +1,17 @@
+﻿::
+:: Author      : Pedro Martins
+::             : https://www.linkedin.com/in/rechousa
+::             : https://twitter.com/rechousapt
+::             : http://www.pedromartins.pt
+:: Project     : Scripts & Snippets
+:: Create date : 2019-09-19 15:48:38
+:: Description : Starts a new command line using a different user context
+:: Remarks     : 1) Replace your_new_user_account variable
+::
+@echo off
+
+set user_account="your_new_user_account"
+
+cls
+
+runas /noprofile /user:%user_account% cmd
