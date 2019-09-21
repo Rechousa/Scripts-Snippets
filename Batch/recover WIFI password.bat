2fa5d0d651902830d7945aa9676c2ae9be716da4
Pedro Martins <rechousa@gmail.com>
2019-09-21 16:48:30 +0100

    Added batch script to recover the password of an existing WIFI network

---
+++ Batch/recover WIFI password.bat
@@ -0,0 +1,21 @@
+﻿::
+:: Author      : Pedro Martins
+::             : https://www.linkedin.com/in/rechousa
+::             : https://twitter.com/rechousapt
+::             : http://www.pedromartins.pt
+:: Project     : Scripts & Snippets
+:: Create date : 2019-09-21 16:46:38
+:: Description : Recovers a WIFI password on the existing computer
+:: Remarks     : The user must enter the WIFI name correctly
+::
+@echo off
+
+cls
+
+netsh wlan show profile
+
+set /p wifi="Enter the WIFI name to recover the password: "
+
+netsh wlan show profile name=%wifi% key=clear
+
+pause
